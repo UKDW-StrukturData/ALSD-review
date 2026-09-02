b11315459c1e969c6c76d8ed48b36a15d96f5ffa
ElkoDuka <eliqohen@gmail.com>
2026-09-02 12:17:58 +0700

    jhj

--- jawaban/soal2.py
+++ jawaban/soal2.py
@@ -1,1 +1,21 @@
+<<<<<<< Updated upstream
 import soal1
+=======
+def cari_grade (nilai):
+    if nilai <50:
+        print(f"nilai {nilai} -> Grade E")
+    elif nilai <= 59:
+        print(f"nilai {nilai} -> Grade D")
+    elif nilai <= 69:
+        print(f"nilai {nilai} -> Grade C")
+    elif nilai <= 84:
+        print(f"nilai {nilai} -> Grade B")
+    elif nilai <= 100:
+        print(f"nilai {nilai} -> Grade A")
+
+cari_grade (95.5)
+cari_grade (80)
+cari_grade (63)
+cari_grade (55)
+cari_grade (30)
+>>>>>>> Stashed changes
