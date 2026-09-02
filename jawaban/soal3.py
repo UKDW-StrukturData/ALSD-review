b8b5adce2d34d506b0c103278febe1400ce0fa05
banny05 <priskilahaba807@gmail.com>
2026-09-02 12:19:00 +0700

    Update soal3.py

--- jawaban/soal3.py
+++ jawaban/soal3.py
@@ -1,9 +1,11 @@
 def hitung_rata2(list_nilai):
     xyz= 0
+    abc =0
     for i in list_nilai:
-        xyz =+ 1
-        print(f"Nilai ke-1:{i}")
-        ratarata = xyz / len(list_nilai)
-        print(f"rata-rata:{ratarata}")
-nilai = [90, 78, 80, 85, 95]
-hitung_rata2(lis)
+        xyz +=1
+        abc += 1
+        print(f"Nilai ke-{abc}:{i}")
+    ratarata = xyz / len(list_nilai)
+    return f"rata-rata:{ratarata}"
+
+print(hitung_rata2(list_nilai= [90, 78, 80, 85, 95]))
